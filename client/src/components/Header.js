4eeee58063f23281124220986b42d7429ee8e5da
Niranjan Praveen <niranjanbpraveen@gmail.com>
2025-05-12 22:48:22 +0530

    Updated Files

---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <header className="flex items-center justify-between py-4 w-full shadow-sm z-50 section-p">
+      
+    </header>
+  );
+}
