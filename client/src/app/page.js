1f53a782e55260bc07d74ac3aeb2e307a1c7ff56
Niranjan Praveen <niranjanbpraveen@gmail.com>
2025-05-12 22:55:23 +0530

    Updated Files

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer'
 import Header from '@/components/Header'
 import React from 'react'
 
@@ -5,6 +6,7 @@ const Home = () => {
   return (
     <div className='flex flex-col overflow-hidden'>
         <Header/>
+        <Footer/>
     </div>
   )
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="flex items-center justify-between py-4 w-full shadow-sm z-50 section-p">
-      
+      Home
     </header>
   );
 }
