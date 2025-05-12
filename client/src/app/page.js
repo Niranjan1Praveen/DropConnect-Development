d81cee46e9be2e25193603fcc351bec43f655170
Niranjan Praveen <niranjanbpraveen@gmail.com>
2025-05-12 23:53:59 +0530

    Updated Files

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,11 +1,15 @@
+import Faqs from '@/components/Faqs'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import Hero from '@/components/Hero'
 import React from 'react'
 
 const Home = () => {
   return (
     <div className='flex flex-col overflow-hidden'>
         <Header/>
+        <Hero/>
+        <Faqs/>
         <Footer/>
     </div>
   )
--- client/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer = () => {
-  return (
-    <div>THIS IS FOOTER</div>
-  )
-}
-
-export default Footer
