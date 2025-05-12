c53e2367ab94aa7178a3e832f4f723ca9138c407
Niranjan Praveen <niranjanbpraveen@gmail.com>
2025-05-12 23:05:32 +0530

    UPDATED BY NIRANJAN

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,6 +7,7 @@ export default function Header() {
   return (
     <header className="flex items-center justify-between py-4 w-full shadow-sm z-50 section-p">
       Home
+      THIS IS MY HEADER
     </header>
   );
 }
