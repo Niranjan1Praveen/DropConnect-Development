d94e88b3e5ef114815e51fa0a17211d7e99ceec7
VAIBHAV <vaibavjain62@gmail.com>
2025-05-14 14:10:31 +0530

    Updated integration

--- client/src/assets/data/integrations.js
+++ client/src/assets/data/integrations.js
@@ -1,23 +1,15 @@
 import figmaIcon from "@/assets/images/figma-logo.svg"
-import notionIcon from "@/assets/images/notion-logo.svg"
-import slackIcon from "@/assets/images/slack-logo.svg"
-import relumeIcon from "@/assets/images/relume-logo.svg"
+import shadcnIcon from "@/assets/images/shadcn-logo.svg"
 import framerIcon from "@/assets/images/framer-logo.svg"
 import githubIcon from "@/assets/images/github-logo.svg"
+import googleMapsIcon from "@/assets/images/googlemaps-logo.png"
+import linkedinIcon from "@/assets/images/linkedin-logo.png"
 
 export default [
     { name: "Figma", icon: figmaIcon, description: "Figma is a collaborative interface design tool." },
-    { name: "Notion", icon: notionIcon, description: "Notion is an all-in-one workspace for notes and docs." },
-    { name: "Slack", icon: slackIcon, description: "Slack is a powerful team communication platform." },
-    { name: "Relume", icon: relumeIcon, description: "Relume is a no-code website builder and design system." },
+    { name: "LinkedIn", icon: linkedinIcon, description: "LinkedIn adds volunteer badges and professional endorsements." },
+    { name: "Google Maps", icon: googleMapsIcon, description: "Google Maps enables location-based event and volunteer matching." },
+    { name: "Shadcn/ui", icon: shadcnIcon, description: "Shadcn/ui offers accessible, customizable UI components for React." },
     { name: "Framer", icon: framerIcon , description: "Framer is a professional website prototyping tool." },
     { name: "GitHub", icon: githubIcon, description: "GitHub is the leading platform for code collaboration." },
 ];
-// export default [
-//     { name: "Google Maps", icon: googleMapsIcon, description: "Google Maps enables location-based event and volunteer matching." },
-//     { name: "LinkedIn", icon: linkedinIcon, description: "LinkedIn adds volunteer badges and professional endorsements." },
-//     { name: "Metabase", icon: metabaseIcon, description: "Metabase powers CSR dashboards with real-time impact insights." },
-//     { name: "Firebase Auth", icon: firebaseIcon, description: "Firebase Auth secures user login with role-based access." },
-//     { name: "GitHub", icon: githubIcon, description: "GitHub supports collaboration and version control for developers." },
-//     { name: "Framer", icon: framerIcon, description: "Framer is a visual prototyping tool for interactive web interfaces." },
-//   ];
